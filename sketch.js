f6a41becfe17bcc943595ff0806d1b761db78262
KUWARVIR <damanpreet.kharbanda@gmail.com>
2020-06-03 20:48:53 +0530

    api call

--- sketch.js
+++ sketch.js
@@ -5,13 +5,14 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var bgImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
 
+var score=0;
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   getBackGround();
 }
 
 function setup(){
@@ -45,18 +46,25 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(bgImg)
+    background(bgImg);
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("score: "+score,width-300,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +93,18 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+ async function  getBackGround(){
+  var response= await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJ= await response.json();
+  var dateTime=responseJ.datetime;
+  var hour= dateTime.slice(11,13);
+
+  if(hour>=06 && hour<=18){
+  bg="sprites/bg.png";
+  }else{
+  bg="sprites/bg2.jpg";
+  }
+  bgImg=loadImage(bg);
 }
